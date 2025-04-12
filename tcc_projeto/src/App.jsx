d2e1232e3da0ccd7578e9e08d75cf3966b4003b8
KalebSilva1002 <kaleb.braz@gmail.com>
2025-04-12 02:24:24 -0300

    Adicionando componente Footer e conteúde de exemplo

--- tcc_projeto/src/App.jsx
+++ tcc_projeto/src/App.jsx
@@ -1,21 +1,111 @@
 import { useState } from 'react';
-import Header from './components/Header';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
-
-
 function App() {
-  
+
 
   return (
     <>
-       <Router>
-      <Header />
-      <Routes>
-       
-      </Routes>
-    </Router>
+      <Router>
+        <Header />
+        
+        <main className='main-app-test'>
+          <h1>Teste de conteúdo</h1>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus perspiciatis ullam tempora sapiente <br />
+              atque fuga ab explicabo quam ducimus in! Doloremque id dolores cupiditate dolorem ratione totam sint dolore <br />
+              saepe?</p>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus perspiciatis ullam tempora sapiente <br />
+              atque fuga ab explicabo quam ducimus in! Doloremque id dolores cupiditate dolorem ratione totam sint dolore <br />
+              saepe?</p>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus perspiciatis ullam tempora sapiente <br />
+              atque fuga ab explicabo quam ducimus in! Doloremque id dolores cupiditate dolorem ratione totam sint dolore <br />
+              saepe?</p>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus perspiciatis ullam tempora sapiente <br />
+              atque fuga ab explicabo quam ducimus in! Doloremque id dolores cupiditate dolorem ratione totam sint dolore <br />
+              saepe?</p>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus perspiciatis ullam tempora sapiente <br />
+              atque fuga ab explicabo quam ducimus in! Doloremque id dolores cupiditate dolorem ratione totam sint dolore <br />
+              saepe?</p>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus perspiciatis ullam tempora sapiente <br />
+              atque fuga ab explicabo quam ducimus in! Doloremque id dolores cupiditate dolorem ratione totam sint dolore <br />
+              saepe?</p>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus perspiciatis ullam tempora sapiente <br />
+              atque fuga ab explicabo quam ducimus in! Doloremque id dolores cupiditate dolorem ratione totam sint dolore <br />
+              saepe?</p>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus perspiciatis ullam tempora sapiente <br />
+              atque fuga ab explicabo quam ducimus in! Doloremque id dolores cupiditate dolorem ratione totam sint dolore <br />
+              saepe?</p>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus perspiciatis ullam tempora sapiente <br />
+              atque fuga ab explicabo quam ducimus in! Doloremque id dolores cupiditate dolorem ratione totam sint dolore <br />
+              saepe?</p>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus perspiciatis ullam tempora sapiente <br />
+              atque fuga ab explicabo quam ducimus in! Doloremque id dolores cupiditate dolorem ratione totam sint dolore <br />
+              saepe?</p>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus perspiciatis ullam tempora sapiente <br />
+              atque fuga ab explicabo quam ducimus in! Doloremque id dolores cupiditate dolorem ratione totam sint dolore <br />
+              saepe?</p>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus perspiciatis ullam tempora sapiente <br />
+              atque fuga ab explicabo quam ducimus in! Doloremque id dolores cupiditate dolorem ratione totam sint dolore <br />
+              saepe?</p>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus perspiciatis ullam tempora sapiente <br />
+              atque fuga ab explicabo quam ducimus in! Doloremque id dolores cupiditate dolorem ratione totam sint dolore <br />
+              saepe?</p>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus perspiciatis ullam tempora sapiente <br />
+              atque fuga ab explicabo quam ducimus in! Doloremque id dolores cupiditate dolorem ratione totam sint dolore <br />
+              saepe?</p>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus perspiciatis ullam tempora sapiente <br />
+              atque fuga ab explicabo quam ducimus in! Doloremque id dolores cupiditate dolorem ratione totam sint dolore <br />
+              saepe?</p>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus perspiciatis ullam tempora sapiente <br />
+              atque fuga ab explicabo quam ducimus in! Doloremque id dolores cupiditate dolorem ratione totam sint dolore <br />
+              saepe?</p>
+          </div>
+          <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus perspiciatis ullam tempora sapiente <br />
+              atque fuga ab explicabo quam ducimus in! Doloremque id dolores cupiditate dolorem ratione totam sint dolore <br />
+              saepe?</p>
+          </div>
+        </main>
+        <Footer />
+        <Routes>
+
+        </Routes>
+      </Router>
     </>
   )
-}export default App
+} export default App
