11cbed4dc57a1a8cd90bbbefe33d2c9cc5bad8b1
Kaleb Bastos Braz Da Silva <kaleb.braz@gmail.com>
2025-04-13 03:00:03 -0300

    Término do Footer! Todas as alterações completas.

--- tcc_projeto/src/components/Footer.jsx
+++ tcc_projeto/src/components/Footer.jsx
@@ -1,43 +1,74 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
 import '../css/Footer.css';
-import { FaChevronDown } from "react-icons/fa";
+import { HiOutlineEnvelope } from "react-icons/hi2";
+import { FaWhatsapp, FaInstagram, FaLinkedin } from "react-icons/fa";
+import FooterOnda from '../assets/Footer_onda.jsx';
 
 function Footer() {
-
     return (
-        <footer className='footer'>
+        <>
+            <FooterOnda></FooterOnda>
+            <footer className='footer'>
 
-            <nav className='nav-footer'>
-                <ul>
-                    <li className='dropdown'>
-                        <span className='nav-text'> Serviços <FaChevronDown className='seta' /></span>
-                        <ul className="dropdown-menu">
-                            <li><Link to>Nutrição Esportiva</Link></li>
-                            <li><Link to>Nutrição Pediátrica</Link></li>
-                            <li><Link to>Nutrição Clínica</Link></li>
-                            <li><Link to>Emagrecimento e Obesidade</Link></li>
-                            <li><Link to>Intolerâncias Aliemtares</Link></li>
+                <div className='footer-container'>
+                    <nav className='nav-footer-um'>
+
+                        <ul className='list-links'>
+                            <h3 className='list-text-links'> Links Úteis </h3>
+
+                            <li><Link to id='li-design'>Receitas Saudáveis</Link></li>
+                            <li><Link to id='li-design'>Dicas Alimentares</Link></li>
+                            <li><Link to id='li-design'>Nutrição Clínica</Link></li>
+                            <li><Link to id='li-design'>Sobre a  Dra. Natália Simanoviski</Link></li>
+                            <li><Link to id='li-design'>Fale Comigo</Link></li>
                         </ul>
-                    </li>
 
-                    <li className='dropdown'>
-                        <span className='nav-text'> Calculadoras<FaChevronDown className='seta'></FaChevronDown></span>
-                        <ul className="dropdown-menu">
-                            <li><Link to>Calorias e Nutrientes</Link></li>
-                            <li><Link to>Gastos Caloricos</Link></li>
-                            <li><Link to>IMC e Peso Ideal</Link></li>
-                            <li><Link to>Consumo diário de Água</Link></li>
+                        <ul className='list-links'>
 
+                            <h3 className='list-text-servicos'> Serviços </h3>
+                            <li><Link to id='li-design'>Nutrição Esportiva</Link></li>
+                            <li><Link to id='li-design'>Nutrição Pediátrica</Link></li>
+                            <li><Link to id='li-design'>Nutrição Clínica</Link></li>
+                            <li><Link to id='li-design'>Emagrecimento e Obesidade</Link></li>
+                            <li><Link to id='li-design'>Intolerâncias Aliemtares</Link></li>
                         </ul>
-                    </li>
 
-                </ul>
-            </nav>
+                        <ul className='list-links'>
 
-        </footer>
+                            <h3 className='list-text-calculadoras'> Calculadoras</h3>
+                            <li><Link to id='li-design'>Calorias e Nutrientes</Link></li>
+                            <li><Link to id='li-design'>Gastos Caloricos</Link></li>
+                            <li><Link to id='li-design'>IMC e Peso Ideal</Link></li>
+                            <li><Link to id='li-design'>Consumo diário de Água</Link></li>
+                        </ul>
+                    </nav>
 
+                    <div className='section-footer'>
+                        <div className='section-pai'>
+                            <div className='button-footer'>
+                                <button>Agendar Consulta</button>
+                            </div>
+                            <div className='icons-footer'>
+                                <FaInstagram className='icon-footer'></FaInstagram>
+                                <FaLinkedin className='icon-footer'></FaLinkedin>
+                                <FaWhatsapp className='icon-footer'></FaWhatsapp>
+                                <HiOutlineEnvelope className='icon-footer'></HiOutlineEnvelope>
+                            </div>
+                        </div>
+                        <div className='logo-footer'>
+                            <img src="./src/assets/Logo.png" alt="Logo" width={200} />
+                            <p id='crn'>CRN : 37892 </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='direitos'>
+                    <p id='text-direitos-um'>Alimentação saudável é um investimento no seu bem-estar!</p>
+                    <p id='text-direitos-dois'>© 2025 Natália Simanoviski | Todos os direitos reservados.</p>
+                </div>
+            </footer>
+        </>
     )
 }
-
 export default Footer;
