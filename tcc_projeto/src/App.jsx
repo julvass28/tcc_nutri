b4c0188c9178bbc51d5164d035afafb6dedbd07b
Kaleb Bastos Braz Da Silva <kaleb.braz@gmail.com>
2025-04-14 01:22:03 -0300

    Novos imports no App.jsx adicionado

--- tcc_projeto/src/App.jsx
+++ tcc_projeto/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
+import IMCCalculator from './components/IMCCalculator.jsx';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -12,7 +13,7 @@ function App() {
     <>
       <Router>
         <Header />
-
+        <IMCCalculator />
         <Footer />
         <Routes>
 
