f4f9dbf05a992e18fa4b50c635f23bd5e3da4e94
Julia Vasconcellos <julia.oliveira563@etec.sp.gov.br>
2025-04-14 22:37:35 -0300

    Ajutes home

--- tcc_projeto/src/pages/Home.jsx
+++ tcc_projeto/src/pages/Home.jsx
@@ -3,6 +3,11 @@ import onda from '../assets/img_png/onda.png'
 import '../css/Home.css'
 import foto from '../assets/img_png/foto_nutri.png';
 import Button from '../components/Button';
+import IconClinic from '../assets/img_png/icon-clinic.png';
+import IconPediatria from '../assets/img_png/icon-pediatria.png';
+import IconEsportivo from '../assets/img_png/icon-esportivo.png'
+import IconEmagrecer from '../assets/img_png/icon-emagrecimento.png'
+import IconIntolerancia from '../assets/img_png/icon-intolerancia.png'
 
 
 
@@ -16,6 +21,7 @@ function Home() {
                 <div className="inicio">
                     <img src={onda} alt="" className="onda" />
                     <h1 className="text-principal">Te ajudo a transformar sua alimentação <br /> de forma leve e sem complicações!</h1>
+
                 </div>
 
                 <div className="descricao">
@@ -32,16 +38,49 @@ function Home() {
                 </div>
             </div>
 
-            <div className="especialidades">
-           <h2>Qual o seu objetivo?</h2>
+            <div>
+                <h2 className="especialidade">Qual o seu objetivo?</h2>
+                <hr className="min-linha"/>
 
-          <div className="categorias">
-        
-          </div>
-          
+                <div className="categorias">
+
+
+                    <div className="linha">
+
+                        <div className="categoria">
+                            <img src={IconClinic} alt="" />
+                            <p>Nutrição Clínica</p>
+                        </div>
+
+                        <div className="categoria">
+                            <img src={IconPediatria} alt="" />
+                            <p>Nutrição Pediátrica</p>
+                        </div>
+
+                        <div className="categoria">
+                            <img src={IconEsportivo} alt="" />
+                            <p>Nutrição Esportiva</p>
+                        </div>
+
+                    </div>
+
+
+                    <div className="linha linha-menor">
+
+                        <div className="categoria">
+                            <img src={IconEmagrecer} alt="" />
+                            <p>Emagrecimento e Obesidade</p>
+                        </div>
+
+                        <div className="categoria">
+                            <img src={IconIntolerancia} alt="" />
+                            <p>Intolerâncias <br /> Alimentares</p>
+                        </div>
+
+                    </div>
+
+                </div>
             </div>
-
-
 
 
         </>
