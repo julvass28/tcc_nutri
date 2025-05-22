27cedc5272fd3caee69fff58ffae3398921a66c1
Giovana <gi.azevedo08@gmail.com>
2025-05-21 22:56:45 -0300

    criação do DicasNutri e Sobre

--- tcc_projeto/src/App.jsx
+++ tcc_projeto/src/App.jsx
@@ -1,25 +1,30 @@
-import { useState } from 'react';
-// import Header from './components/Header.jsx';
-// import Footer from './components/Footer.jsx';
-// import IMCCalculator from './pages/IMCCalculator.jsx';
-import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+// import Footer from './components/Footer';
+// import IMCCalculator from './pages/IMCCalculator';
+// import Sobre from './pages/Sobre';
 import PaginaRota from './pages/PaginaRota';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import './App.css';
 
-function App() {
+import Contato from './pages/Contato';
 
-
+export default function App() {
   return (
-  <>
-      <Router>
-        {/* <Header /> */}
-        {/* <IMCCalculator /> */}
-        
-        <Routes>
-        <Route path="/especialidade/:tipo" element={<PaginaRota />} />
-        </Routes>
-        {/* <Footer /> */}
-      </Router>
-    </>
-  )
-} export default App
+    <Router>
+      <Header />
+      {/* <Route path="/" element={<IMCCalculator />} /> */}
+
+     
+      <Routes>
+       <Route path="/especialidade/:tipo" element={<PaginaRota />} /> 
+       {/* <Route path="/sobre" element={<Sobre />} />  */}
+       <Route path="/contato" element={<Contato />} /> 
+      
+      </Routes>
+
+
+      {/* <Footer /> */}
+    </Router>
+  );
+}
