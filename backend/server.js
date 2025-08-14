3b2211d2d42ef4be06074d191290f43480fc4dc2
Kaleb Bastos Braz Da Silva <kaleb.braz@gmail.com>
2025-08-14 09:32:15 -0300

    deployando o back mais uma vez!

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,14 @@ const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-const FRONTEND_URL = process.env.FRONTEND_URL || 'https://tcc-nutri.vercel.app/';
+const FRONTEND_URL = (process.env.FRONTEND_URL || 'https://tcc-nutri.vercel.app').replace(/\/$/, '');
 app.use(cors({
-  origin: FRONTEND_URL === 'https://tcc-nutri.vercel.app/' ? true : [FRONTEND_URL],
+  origin: [FRONTEND_URL],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
+app.options('*', cors());
 
 app.use(express.json());
 app.use(helmet());
